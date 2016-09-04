90f69322e9320d911689dc99d053160ea2c93eec
Dan Rye <Dan@RyeDesigns.com>
2016-09-04 09:02:18 -0600

    configuration

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,9 +4,9 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = '';
+$database = 'hlinterf_ss';
 
-require_once('conf/ConfigureFromEnv.php');
+require_once("conf/ConfigureFromEnv.php");
 
 // Set the site locale
 i18n::set_locale('en_US');
